bfa16c5177ceee072175b22de67aa47629cbbb8c
Infini Tech <janselscruz@gmail.com>
2025-12-07 13:46:23 -0400

    Fixes

--- testRedis.ts
+++
@@ -1,6 +0,0 @@
-import IORedis from "ioredis";
-const redis = new IORedis(process.env.REDIS_URL!);
-redis.ping().then((res) => {
-  console.log("✅ Redis responde:", res);
-  redis.disconnect();
-});
